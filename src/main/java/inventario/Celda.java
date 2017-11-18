756366589b327118294df70c7cd4d93d75c36e2f
Valeria Silvestri <valeria.ayelen@gmail.com>
2017-11-18 12:28:30 -0300

    Cambios Mochila

--- src/main/java/inventario/Celda.java
+++ src/main/java/inventario/Celda.java
@@ -27,7 +27,7 @@ public class Celda extends JPanel {
 
 
     public Celda(Item item, PaquetePersonaje paquetePersonaje) throws IOException {
-		this.item = item.getFoto();
+		this.item = item.getFotoBI();
 		it = item;
 		this.paquetePersonaje = paquetePersonaje;
 		label = new JLabel(new ImageIcon(this.item.getScaledInstance(49, 49, Image.SCALE_DEFAULT)));
