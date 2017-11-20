d33443a4141998d6ed5b7be5613c489d86ea26a6
Valeria Silvestri <valeria.ayelen@gmail.com>
2017-11-19 23:36:08 -0300

    Hijos de TrickHandler

--- src/main/java/chat/BigDaddyHandler.java
+++ src/main/java/chat/BigDaddyHandler.java
@@ -1,16 +1,20 @@
 package chat;
 
+import juego.Juego;
+
 public class BigDaddyHandler extends TrickHandler {
 	public final static String miComando = "bigdaddy";
 	
 	public BigDaddyHandler(TrickHandler sucesor) {
 		super(sucesor);
 	}
-	public void ejecutar() {
-		System.out.println("Big daddy");
-	}
 	@Override
 	public boolean puedoSoportarlo(String comando) {
 		return this.miComando.equals(comando);
+	}
+	@Override
+	protected void ejecutar(Juego juego) {
+		// TODO Auto-generated method stub
+		
 	}
 }
