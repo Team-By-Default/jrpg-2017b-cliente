573dbd987dd79ad49757c8a2c9d055ca80cfa5d6
Valeria Silvestri <valeria.ayelen@gmail.com>
2017-11-19 23:18:53 -0300

    Invisible trick

--- src/main/java/chat/InvisibleHandler.java
+++ src/main/java/chat/InvisibleHandler.java
@@ -1,21 +1,26 @@
 package chat;
 
+import juego.Juego;
+import mensajeria.PaquetePersonaje;
+
 public class InvisibleHandler extends TrickHandler {
 	public final static String miComando = "invisible";
 
 	public InvisibleHandler(TrickHandler sucesor) {
 		super(sucesor);
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public boolean puedoSoportarlo(String comando) {
 		return this.miComando.equals(comando);
 	}
-
+	
 	@Override
-	public void ejecutar() {
-		// TODO Auto-generated method stub
+	protected void ejecutar(Juego juego) {
+		PaquetePersonaje pj = juego.getCliente().getPaquetePersonaje();
+		pj.setInvisible(!pj.isInvisible());
+		juego.getCliente().setPaquetePersonaje(pj);
+		
 		System.out.println("I cant see you");
 	}
 }
--- src/main/java/chat/TrickHandler.java
+++ src/main/java/chat/TrickHandler.java
@@ -1,19 +1,34 @@
 package chat;
 
+import java.io.IOException;
+import javax.swing.JOptionPane;
+import com.google.gson.Gson;
+import juego.Juego;
+import mensajeria.Comando;
+
 public abstract class TrickHandler {
 	protected TrickHandler sucesor;
+	private final Gson gson;
 	
 	public TrickHandler(TrickHandler sucesor) {
 		this.sucesor = sucesor;
+		this.gson = new Gson();
 	}
 	
-	public void ejecutarComando(String comando) {
-		if(puedoSoportarlo(comando))
-			ejecutar();
+	public void ejecutarComando(String comando, Juego juego) {
+		if(puedoSoportarlo(comando)) {
+			ejecutar(juego);
+			juego.getCliente().getPaquetePersonaje().setComando(Comando.CHEAT);
+			try {
+				juego.getCliente().getSalida().writeObject(gson.toJson(juego.getCliente().getPaquetePersonaje()));
+			} catch (IOException e) {
+				JOptionPane.showMessageDialog(null, "Error al aplicar el truco... ¡Dejá de hacer trampa!");
+			}
+		}
 		else
-			sucesor.ejecutarComando(comando);
+			sucesor.ejecutarComando(comando, juego);
 	}
 	
-	public abstract boolean puedoSoportarlo(String comando);
-	public abstract void ejecutar();
+	protected abstract boolean puedoSoportarlo(String comando);
+	protected abstract void ejecutar(Juego juego);
 }
